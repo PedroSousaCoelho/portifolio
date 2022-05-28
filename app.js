a96e5e123140c3d6d5d4a1115b6df7827264c3ca
Pedro <91393596+PedroSousaCoelho@users.noreply.github.com>
2022-05-28 20:13:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const menu = document.querySelector('.menu')
+const trabalho = document.querySelector('.trabalho')
+const projetos = document.querySelector('.projetos')
+const contato = document.querySelector('.contato')
